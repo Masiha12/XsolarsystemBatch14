ca94bab2662996eb78c54396d5e82bd4f434fe0a
Masiha12 <abdullahmasiha44@gmail.com>
2022-12-19 14:30:38 -0500

    Feature sun has been added but it has some errors need to be resolved

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,10 @@ SolarSystem xx =new SolarSystem();
 xx.moon=1;
 xx.sun=1;
 xx.planet=2;
+
+FeatureSun Xeron=new FeatureSun();
+Xeron.Heat="12324234";
+Xeron.size=1221334234;
+Xeron.Temperature="3434";
     }
 }
